41901aa03632e20270604043540de54a7a7f6c1d
felipepitoko <lip-sousa@hotmail.com>
2024-10-14 00:33:24 -0300

    add allowed host

--- demo/settings.py
+++ demo/settings.py
@@ -112,6 +112,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+ALLOWED_HOSTS =[
+    'felipepitoko.pythonanywhere.com'
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
