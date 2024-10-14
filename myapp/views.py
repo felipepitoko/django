fba6f2cca8997630a2ae1ba4e2c0a3192f23c42b
felipepitoko <lip-sousa@hotmail.com>
2024-10-13 22:46:58 -0300

    todo list simple

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import TodoItem
 # Create your views here.
 
 def home(request):
     return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def todo(request):
+    items = TodoItem.objects.all() #pega todos os itens do banco de dados
+    return render(request, "todos.html", {"todos": items})
